981a36726dc7d3de4f11a869846ed264d409e866
suneeksha <43674820+suneeksha@users.noreply.github.com>
2021-05-09 14:26:33 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,7 +6,6 @@ setwd("C:/Users/SUNEEKSHA/Desktop/EDAprojectweek1")
 pwrdeetee <- data.table::fread(input = "household_power_consumption.txt"
                              , na.strings="?"
 )
-
 # Histogram is prevented from printing
 pwrdeetee[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
